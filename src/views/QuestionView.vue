21bafc144609be79ab0a01b2c22af80aadfabbd5
Hao <h.dong1998@hotmail.com>
2024-01-30 16:15:16 +0100

    Add random coordinates

--- src/views/QuestionView.vue
+++ src/views/QuestionView.vue
@@ -1,40 +1,78 @@
 <script lang="ts" setup>
-import {ref} from "vue";
+import {onMounted, ref} from "vue";
+
+enum Axis {
+  "x",
+  "y"
+}
 
 const noInput = ref<HTMLDivElement | null>();
+const content = ref<HTMLDivElement | null>(null);
+const bodyWidth = ref<number>(0)
+const bodyHeight = ref<number>(0)
+const safeMargin = 50
+
 
 function moveInput(event: Event) {
   if (noInput.value) {
-    const randomXCoordination = generateRandomCoordination()
-    const randomYCoordination = generateRandomCoordination()
-    console.log(randomXCoordination, randomXCoordination)
+    const randomXCoordination = preventCoordinatesOverflow(generateRandomCoordinates(Axis.x), Axis.x)
+    const randomYCoordination = preventCoordinatesOverflow(generateRandomCoordinates(Axis.y), Axis.y)
+    console.log(`x: ${randomXCoordination}, y: ${randomYCoordination}`)
     noInput.value.style.transform = `translate(${randomXCoordination}px, ${randomYCoordination}px)`
   }
 }
 
-function randomMultiplier(): number {
-  return Math.floor(Math.random() * 10000) + 1;
+function randomMultiplier(axis: Axis): number {
+  if (axis === Axis.x) {
+    return Math.floor(Math.random() * bodyWidth.value);
+  }
+  return Math.floor(Math.random() * bodyHeight.value);
 }
 
-function generateRandomCoordination() {
-  return Math.floor(Math.random() * randomMultiplier()) + 1
+function generateRandomCoordinates(axis: Axis) {
+  return Math.floor(Math.random() * randomMultiplier(axis)) + 1
 }
+
+function preventCoordinatesOverflow(coordinates: number, axis: Axis): number {
+  if (axis === Axis.x) {
+    if (coordinates > bodyWidth.value) {
+      return bodyWidth.value
+    }
+    return coordinates
+  }
+
+  if (coordinates > bodyHeight.value) {
+    return bodyHeight.value
+  }
+  return coordinates
+}
+
+onMounted(() => {
+  if (content.value) {
+    const client = content.value.getBoundingClientRect()
+    bodyHeight.value = client.height - safeMargin
+    bodyWidth.value = client.width - safeMargin
+  }
+
+})
 </script>
 
 <template>
-  <h1>Do you want to be my valentine?</h1>
-  <img alt="A cat holding a rose" src="../assets/images/cat-giving-rose.jpeg">
+  <div ref="content" class="h-full">
+    <h1>Do you want to be my valentine?</h1>
+    <img alt="A cat holding a rose" src="../assets/images/cat-giving-rose.jpeg">
 
-  <div>
-    <input id="yes" class="scale-150" name="valentine" type="radio">
-    <label class="text-5xl" for="yes">Yes</label>
-  </div>
-  <div ref="noInput" @click="moveInput">
-    <input id="no" name="valentine" type="radio">
-    <label for="no">No</label>
-  </div>
+    <div>
+      <input id="yes" class="scale-150" name="valentine" type="radio">
+      <label class="text-5xl" for="yes">Yes</label>
+    </div>
+    <div ref="noInput" @click="moveInput">
+      <input id="no" name="valentine" type="radio">
+      <label for="no">No</label>
+    </div>
 
-  <button>Submit</button>
+    <!--  <button @click="">Submit</button>-->
+  </div>
 </template>
 
 <style scoped>
