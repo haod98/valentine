1458c6be61338bf3ae8f60f199a8164170856cad
Hao <h.dong1998@hotmail.com>
2024-02-03 14:56:37 +0100

    Add styling when passing

---
+++ src/components/HeadingComponent.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <p :class="$attrs" class="text-center font-secondary uppercase font-bold text-5xl text-[#fb8ba2]">
+    <slot></slot>
+  </p>
+</template>
+
+<style scoped>
+
+</style>
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -3,12 +3,14 @@
 import {ref} from "vue";
 import {useRouter} from "vue-router";
 import PrimaryButton from "@/components/PrimaryButton.vue";
+import HeadingComponent from "@/components/HeadingComponent.vue";
 
-const typedPassword = ref<string>("")
+const typedPassword = ref<string>("100523")
 const passwordIsWrong = ref<boolean>(false)
 // Don't look at the source code for the password >:(
 const correctPassword = '100523'
 const router = useRouter()
+const showCorrect = ref(false)
 
 function submit() {
   if (correctPassword !== typedPassword.value) {
@@ -16,33 +18,39 @@ function submit() {
     return
   }
   passwordIsWrong.value = false
-  router.push({name: 'question'})
+  showCorrect.value = true
+  setTimeout(() => {
+    router.push({name: 'question'})
+  }, 5000)
 }
 
 </script>
 
 
 <template>
-  <div class="pt-10">
+  <div v-if="!showCorrect" class="pt-10">
     <div class="flex justify-center flex-col items-center">
       <img alt="" class="mb-3 max-w-[150px] max-h-[150px] min-w-[150px] min-h-[150px]"
            src="/assets/home/1.jpeg">
-      <p v-if="passwordIsWrong">Wrong. How could you get our date wrong? 🥺</p>
     </div>
     <!-- TODO: Add more error messages -->
     <div class="flex flex-col items-center">
-      <h1 class="font-secondary uppercase font-bold text-5xl text-[#fb8ba2]">Top Secret</h1>
+      <HeadingComponent>Top Secret</HeadingComponent>
       <div class="flex-col flex mb-3">
         <label for="password">Whats our anniversary day?</label>
         <input id="password" v-model="typedPassword" class=" rounded-md  p-2.5" type="password"
                @keydown.enter="submit">
       </div>
       <PrimaryButton @emit-click="submit">Confirm</PrimaryButton>
-      <!--      <button @click="submit">Confirm</button>-->
-      <!--      <div class="mt-3">-->
-      <!--        <img alt="" src="/assets/home/cat-dance.gif">-->
-      <!--      </div>-->
+      <p v-if="passwordIsWrong" class="mt-3">How could you get our date wrong? 🥺</p>
     </div>
+  </div>
+  <div v-if="showCorrect" class="h-full flex flex-col justify-center items-center">
+    <HeadingComponent class="text-2xl">That was correct</HeadingComponent>
+    <div class="flex items-center justify-center">
+      <img v-if="true" alt="" src="/assets/home/cat-dance.gif">
+    </div>
+    <HeadingComponent class="text-2xl">You may pass</HeadingComponent>
   </div>
   <p>{{ typedPassword }}</p>
 </template>
