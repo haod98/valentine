a5d2797f0bb7e12f53d2633f38bc6321c837bfd5
Hao <h.dong1998@hotmail.com>
2024-02-03 20:55:14 +0100

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/',
+    base: 'https://haod98.github.io/valentine/',
     plugins: [
         vue(),
     ],
