a16a4a025c540069899070150f301d6456bcb3fe
Hao <h.dong1998@hotmail.com>
2024-02-03 20:42:58 +0100

    Change baseurl

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: "'/<REPO>/'",
+    base: "https://haod98.github.io/valentine",
     plugins: [
         vue(),
     ],
